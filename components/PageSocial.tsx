631011bbb27f32b049ac0b874484a1f91a200128
loopscript <155076734+loopscript@users.noreply.github.com>
2024-03-08 17:20:14 +0100

    Add GitHubShareButton and PageSocial components

---
+++ components/PageSocial.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+
+import cs from 'classnames'
+
+import * as config from '@/lib/config'
+
+import styles from './PageSocial.module.css'
+
+
+
+export const PageSocial: React.FC = () => {
+  return (
+    <div className={styles.pageSocial}>
+      {socialLinks.map((action) => (
+        <a
+          className={cs(styles.action, styles[action.name])}
+          href={action.href}
+          key={action.name}
+          title={action.title}
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <div className={styles.actionBg}>
+            <div className={styles.actionBgPane} />
+          </div>
+
+          <div className={styles.actionBg}>{action.icon}</div>
+        </a>
+      ))}
+    </div>
+  )
+}
