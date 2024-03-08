05fe52a05095f8dc595ec041776794fdbb8f4da9
loopscript <155076734+loopscript@users.noreply.github.com>
2024-03-08 18:04:24 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   twitter: 'SammorMaged',
   github: 'Loopscript',
-  linkedin: 'Maged Sammor',
+  linkedin: 'Maged-Sammor',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
